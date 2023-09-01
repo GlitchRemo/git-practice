dbccb0f792cc26ebcfa7b5325d2e873009b1e82c
Biswajit Sen <biswajitsen@Biswajits-MacBook-Pro.local>
2023-09-01 16:35:36 +0530

    Adds test cases for sub and mul function.

--- test/math-test.js
+++ test/math-test.js
@@ -11,4 +11,22 @@ describe('calculator', () => {
       assert.strictEqual(calculator.add(num1, num2), 2);
     });
   });
+
+  describe('sub', () => {
+    it('should add two numbers', () => {
+      const num1 = 1;
+      const num2 = 1;
+
+      assert.strictEqual(calculator.sub(num1, num2), 0);
+    });
+  });
+
+  describe('mul', () => {
+    it('should add two numbers', () => {
+      const num1 = 1;
+      const num2 = 1;
+
+      assert.strictEqual(calculator.mul(num1, num2), 1);
+    });
+  });
 });
